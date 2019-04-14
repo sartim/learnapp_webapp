c2927105e36104e4f27e485efc9004abd96ca2f2
Tim <sarrtim@gmail.com>
2019-04-14 19:50:57 +0300

    Init

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015', 'stage-3']
+                }
+            }
+        ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+        inject: 'body'
+    }),
+      new CopyWebpackPlugin([
+        { from: 'assets', to: 'assets' },
+        { from: 'bower_components', to: 'bower_components' }
+    ])],
+    devServer: {
+        historyApiFallback: true
+    },
+    externals: {
+        // global app config object
+        config: JSON.stringify({
+            apiUrl: 'http://localhost:5000'
+        })
+    }
+}
