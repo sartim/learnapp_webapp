01c6f033bcd4d91fb6c41961500501ff7b92a49a
Tim <sarrtim@gmail.com>
2019-04-19 19:48:25 +0300

    Hide add quiz link on floating action button

--- src/pages/FabPage/FabPage.jsx
+++ src/pages/FabPage/FabPage.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Script from "../../_helpers/script";
 
 class FabPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {account: ''}
+    }
+
+    componentDidMount() {
+        let account = JSON.parse(localStorage.getItem('user'));
+        this.setState({account: account})
+
+    }
 
     render() {
+        let account = this.state.account;
+        let user_name;
+        let roles;
+        let add_quiz;
+        if (account) {
+            roles = Script.decrypt(account.user.roles);
+            if (roles.includes('TUTOR')) {
+                add_quiz =
+                    <li>
+                        <Link to="/quiz/create" className="btn-floating red" style={{transform: 'scaleY(0.4) scaleX(0.4) translateY(40px) translateX(0px)', opacity: '0'}}>
+                            <i className="material-icons">add</i>
+                        </Link>
+                    </li>;
+            } else {
+                add_quiz = '';
+            }
+        }
         return (
             <div className="fixed-action-btn" style={{bottom: '50px', right: '19px'}}>
             <a className="btn-floating btn-large gradient-45deg-light-blue-cyan gradient-shadow">
@@ -25,11 +53,7 @@ class FabPage extends React.Component {
                   {/*<i className="material-icons">today</i>*/}
                 {/*</Link>*/}
               {/*</li>*/}
-              <li>
-                <Link to="/quiz/create" className="btn-floating red" style={{transform: 'scaleY(0.4) scaleX(0.4) translateY(40px) translateX(0px)', opacity: '0'}}>
-                  <i className="material-icons">add</i>
-                </Link>
-              </li>
+                {add_quiz}
             </ul>
         </div>
         );
